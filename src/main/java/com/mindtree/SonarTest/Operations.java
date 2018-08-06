040ab82285af5b6a42d25cfad108b42775302bf6
Shruti Singh <M1039304@mindtree.com>
2018-08-06 13:57:39 +0530

    first commit

---
+++ src/main/java/com/mindtree/SonarTest/Operations.java
@@ -0,0 +1,20 @@
+package com.mindtree.SonarTest;
+
+public class Operations {
+	
+	public Operations() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public static int Addition(int num1,int num2){
+
+		return num1+num2;
+	}
+
+	public static int Multiplication(int num1,int num2){
+		
+		return num1*num2;
+	}
+
+}
